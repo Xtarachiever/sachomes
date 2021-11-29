89b523436388c6c8417095f618866f81106e5d85
Olagoke Abel Olayinka <olagokeabel@gmail.com>
2021-11-29 15:05:43 +0100

    add animation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import phonepack from "./images/phone-pack.svg";
 import followIcon from "./images/icon/follow-icon.svg";
 import appleStore from "./images/apple-store.svg";
 import googleStore from "./images/google-store.svg";
+import "animate.css";
 // import star from "./images/emoji/star-emoji.svg";
 // import moon from "./images/emoji/moon-emoji.svg";
 // import smile from "./images/emoji/smile-emoji.svg";
@@ -26,13 +27,13 @@ const App = () => {
           </nav>
           <div className='flex flex-col lg:flex-row'>
             <div className='flex flex-col items-center flex-1 jus py-28 mast-left'>
-              <h2 className='max-w-2xl py-2 space-x-2 space-y-1 text-2xl font-bold sm:text-3xl md:text-5xl font-brfirma'>
-                We offer <span className='text-sacgreen'>Safe</span>,
+              <h2 className='max-w-2xl py-2 space-x-2 space-y-1 text-2xl font-bold sm:text-3xl md:text-5xl font-brfirma animate__animated animate__fadeIn animate__slow 2s'>
+                We offer <span className='text-sacgreen '>Safe</span>,
                 <span className='text-sacgreen'> Affordable</span> and
                 <span className='text-sacgreen'> Convenient</span> homes for
                 rent to our users.
               </h2>
-              <div className='flex mt-10'>
+              <div className='flex mt-10 animate__animated animate__fadeIn animate__slow 2s animate__delay-1s'>
                 <Link to='/' className='mr-5'>
                   <img src={googleStore} alt='google-store' className='' />
                 </Link>
@@ -45,7 +46,7 @@ const App = () => {
               <img
                 src={phonepack}
                 alt='sachome app mockup'
-                className='-mt-20 sm:mt-0 sm:object-scale-down h-6/6'
+                className='-mt-20 sm:mt-0 sm:object-scale-down h-6/6 animate__animated animate__bounceInUp animate__delay-2s'
               />
             </div>
           </div>
