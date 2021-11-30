ff04313cfd008caad785dbea91eb1589f0dbbb5e
Olagoke Abel Olayinka <olagokeabel@gmail.com>
2021-11-30 14:15:45 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -13,50 +13,56 @@ import "animate.css";
 
 const App = () => {
   return (
-    <div className='max-h-screen mx-auto overflow-x-hidden bg-no-repeat 2xl:container'>
-      <div className='flex flex-col px-10 sm:bg-cover md:px-15 bg-hero-pattern'>
-        <header className='flex-1'>
-          <nav className='flex items-center justify-between py-5 flex-column'>
-            <h1>SAChomes Logo</h1>
-            <div className='flex flex-row items-center'>
-              <Link to='/'>
-                <img src={followIcon} alt='follow-icon' className='mr-5 w-7' />
-              </Link>
-              <h2 className='text-xl text-sacblack font-brfirma'>Follow</h2>
-            </div>
-          </nav>
-          <div className='flex flex-col lg:flex-row'>
-            <div className='flex flex-col flex-1 jus py-28 mast-left'>
-              <h2 className='max-w-2xl py-2 space-x-2 space-y-1 text-2xl font-bold sm:text-3xl md:text-5xl font-brfirma animate__animated animate__fadeIn animate__slow 2s'>
-                We offer <span className='text-sacgreen '>Safe</span>,
-                <span className='text-sacgreen'> Affordable</span> and
-                <span className='text-sacgreen'> Convenient</span> homes for
-                rent to our users.
-              </h2>
-              <div className='flex mt-10 animate__animated animate__fadeIn animate__slow 2s animate__delay-1s'>
-                <Link to='/' className='mr-5'>
-                  <img src={googleStore} alt='google-store' className='' />
-                </Link>
+    <div className='bg-no-repeat bg-cover bg-hero-pattern'>
+      <div className='min-h-screen mx-auto overflow-x-hidden 2xl:container'>
+        <div className='flex flex-col px-10 sm:bg-cover md:px-15'>
+          <header className='flex-1'>
+            <nav className='flex items-center justify-between py-5 flex-column'>
+              <h1>SAChomes Logo</h1>
+              <div className='flex flex-row items-center'>
                 <Link to='/'>
-                  <img src={appleStore} alt='apple-store' className='' />
+                  <img
+                    src={followIcon}
+                    alt='follow-icon'
+                    className='mr-5 w-7'
+                  />
                 </Link>
+                <h2 className='text-xl text-sacblack font-brfirma'>Follow</h2>
+              </div>
+            </nav>
+            <div className='flex flex-col lg:flex-row'>
+              <div className='flex flex-col items-center flex-1 md:items-start py-28 mast-left'>
+                <h2 className='max-w-2xl py-2 space-x-2 space-y-1 text-2xl font-bold sm:text-3xl md:text-5xl font-brfirma animate__animated animate__fadeIn animate__slow 2s'>
+                  We offer <span className='text-sacgreen '>Safe</span>,
+                  <span className='text-sacgreen'> Affordable</span> and
+                  <span className='text-sacgreen'> Convenient</span> homes for
+                  rent to our users.
+                </h2>
+                <div className='flex mt-10 animate__animated animate__fadeIn animate__slow 2s animate__delay-1s'>
+                  <Link to='/' className='mr-5'>
+                    <img src={googleStore} alt='google-store' className='' />
+                  </Link>
+                  <Link to='/'>
+                    <img src={appleStore} alt='apple-store' className='' />
+                  </Link>
+                </div>
+              </div>
+              <div className='flex items-center justify-center flex-1 mast-right'>
+                <img
+                  src={phonepack}
+                  alt='sachome app mockup'
+                  className='-mt-20 sm:mt-0 sm:object-scale-down h-6/6 animate__animated animate__bounceInUp animate__delay-2s animate__slow 2s '
+                />
               </div>
             </div>
-            <div className='flex items-center justify-center flex-1 mast-right'>
-              <img
-                src={phonepack}
-                alt='sachome app mockup'
-                className='-mt-20 sm:mt-0 sm:object-scale-down h-6/6 animate__animated animate__bounceInUp animate__delay-2s animate__slow 2s '
-              />
-            </div>
-          </div>
-        </header>
-        <footer className='flex-grow-0'>
-          <p className='py-2 mt-10 text-center md:mt-0'>
-            &copy; Copyright 2021 SAChomes
-          </p>
-        </footer>
+          </header>
+        </div>
       </div>
+      <footer className='flex-grow-0'>
+        <p className='py-3 mt-10 text-center md:mt-0'>
+          &copy; Copyright 2021 SAChomes
+        </p>
+      </footer>
     </div>
   );
 };
